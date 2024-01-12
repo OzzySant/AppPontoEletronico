3a55595dd70d8380c696d9a27192a127cad4fccb
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2024-01-12 20:07:17 -0300

    Big Bang Front end

---
+++ frontend/PontoApp/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <div class="row items-center justify-evenly">Teste</div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
