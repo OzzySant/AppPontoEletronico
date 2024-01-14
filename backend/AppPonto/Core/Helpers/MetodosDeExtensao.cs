57b7711c34479f89fa69f19f439743655edd1aa0
Ozeias Santos <ozzyton18@gmail.com>
2024-01-14 15:05:58 -0300

    Metodo conversão de data

---
+++ backend/AppPonto/Core/Helpers/MetodosDeExtensao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core.Helpers
+{
+    public static class MetodosDeExtensao
+    {
+        public static DateTime ConverterStringParaData(this string data, string formato = "dd/MM/yyyy")
+        {
+            return DateTime.ParseExact(data, formato, CultureInfo.InvariantCulture);
+        }
+    }
+}
